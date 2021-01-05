7666f4b06909b00021a09262c7130a4f22a1466e
David Wu <david204code@gmail.com>
2021-01-05 21:53:01 +0000

    Set the state value, create fetch function using async, await and set the response to json() function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
+
+  // state value
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  // fetch function
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    // turn data into json method
+    const newJobs = await response.json();
+  }
+
   return <h2>tabs project setup</h2>
 }
 
