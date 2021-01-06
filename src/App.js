375d21adc867ee12531837f7a4182d5c4d74d04b
David Wu <david204code@gmail.com>
2021-01-06 14:33:47 +0000

    Destructure the data and displaying the first job

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   // state value
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+  // value needed to access the attribute within the array
   const [value, setValue] = useState(0);
 
   // fetch function
@@ -34,11 +35,41 @@ function App() {
     );
   }
 
+  // after loading, destructure coming from the "jobs" state with "value"
+  const {company, dates, duties, title } = jobs[value];
+  // console.log(jobs[value])
+
   return(
     <>
-      <h2>tabs project setup</h2>
+      <section className ="section">
+        <div className ="title">
+          <h2>Experience</h2>
+          <div className ="underline"></div>      
+        </div>
+        <div className ="jobs-center">
+        {/* btn container */}
+        {/* job info */}
+          <article className ="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className ="job-date">{dates}</p>
+            {/* iterate over duties which is an array AND pass in index cause it's a list*/}
+            {duties.map((duty, index) => {
+              return(
+                <div key ={index} className ="job-desc">
+                  <FaAngleDoubleRight
+                    className ="job-icon"
+                  >
+                  </FaAngleDoubleRight>
+                  <p>{duty}</p>
+                </div>  
+              )
+            })}
+          </article>
+        </div>
+      </section>
     </>    
-  ) 
+  ); 
 }
 
 export default App
